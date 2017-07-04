2d3debe6762d1c6415e7b9adf0df775285c5c817
AditiKamboj <aditi.dehar@gmail.com>
2017-07-04 17:48:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-//change the text
-var element=document.getElementbyId('main-text');
-element.innerHtml='New Value';
-
-//move the image
-var img=document.getElementbyId('madi');
-var marginLeft=0;
-function moveRight()
-{marginLeft=marginLeft+10;
-img.style.marginLeft=marginLeft +'px';
-}
-img.onclick=function (){
-    var interval=setInterval(moveRight,100)
-    ;
-    
-};
