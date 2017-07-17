4503a5fdd9cbda9a29c0f3a212a84a18e9ef0e26
AditiKamboj <aditi.dehar@gmail.com>
2017-07-17 17:51:13 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,20 @@
+//counter code
+var button =document.getElementById('counter');
+button.onclick=function (){
+    //create a request object
+    var request =newXMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange=function (){
+        if (request.readyState===XMLHttpRequest.DONE)
+        {//take some action
+        if (request.status===200)
+        {var counter=request.responseText;
+        var span =document.getElementById('count');
+        span.innerHTML=counter.toString();
+        }
+        }
+    };
+    //make the request 
+    request.open('GET','http://aditikamboj.imad.hasura-app.io/counter',true);
+    request.send(null);
+    };
